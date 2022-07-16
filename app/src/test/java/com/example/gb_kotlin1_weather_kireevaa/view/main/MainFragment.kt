4fd9c13b14193b532c361b042a5fa05ff2e141cd
kirsana87 <kireisana@mail.ru>
2022-07-16 17:22:44 +1000

    Создание ссылки на MainViewModel, liveData

--- app/src/test/java/com/example/gb_kotlin1_weather_kireevaa/view/main/MainFragment.kt
+++ app/src/test/java/com/example/gb_kotlin1_weather_kireevaa/view/main/MainFragment.kt
@@ -6,21 +6,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.gb_kotlin1_weather_kireevaa.viewmodel.AppSate
+import com.example.gb_kotlin1_weather_kireevaa.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.example.gb_kotlin1_weather_kireevaa.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-
-
-
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMainBinding.inflate(inflater,container,false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val observer = object : Observer<AppSate> {
+            override fun onChanged(data: AppSate) {
+                renderData(data)
+            }
+
+        }
+        viewModel.getLiveDataWeather().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: AppSate) {
+        when (data) {
+            is AppSate.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainView, data.error.toString(), Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Обновить", {
+                        viewModel.getWeather()
+                    })
+                    .show()
+            }
+            is AppSate.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppSate.Success -> {
+                binding.cityName.text = data.weather.city.name
+                binding.cityCoordinates.text = "${data.weather.city.lat} ${data.weather.city.lon}"
+                binding.feelsLikeValue.text = data.weather.feelsLike.toString()
+                binding.temperatureValue.text = data.weather.temperature.toString()
+                Snackbar.make(binding.mainView, "Все получилось", Snackbar.LENGTH_LONG).show()
+                binding.loadingLayout.visibility = View.GONE
+            }
+        }
     }
 
     companion object {
