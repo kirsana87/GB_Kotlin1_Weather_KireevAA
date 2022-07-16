e2c6217afe9adaa39734fccc6422c376e9f6273f
kirsana87 <kireisana@mail.ru>
2022-07-16 15:59:17 +1000

    начал заново

--- app/src/main/java/com/example/gb_kotlin1_weather_kireevaa/MainActivity.kt
+++ app/src/main/java/com/example/gb_kotlin1_weather_kireevaa/MainActivity.kt
@@ -1,35 +1,16 @@
 package com.example.gb_kotlin1_weather_kireevaa
 
 
-import android.content.Context
-import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.view.View.inflate
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.ButtonBarLayout
-import com.example.gb_kotlin1_weather_kireevaa.weatherlist.WeatherListFragment
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 internal class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.myRoot)
 
-        if(savedInstanceState==null){
-            supportFragmentManager.beginTransaction().replace(R.id.container, WeatherListFragment.newInstance()).commit()
+        setContentView(R.layout.activity_main)
+
         }
     }
 
-}
-
-class ActivityMainBinding {
-
-    var myRoot: Int = 0
-}
--- app/src/main/java/com/example/gb_kotlin1_weather_kireevaa/model/Repository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gb_kotlin1_weather_kireevaa.model
-
-import com.example.gb_kotlin1_weather_kireevaa.domain.Weather
-
-
-interface Repository {
-    fun getListWeather():List<Weather>
-    fun getWeather( lat: Double, lon: Double):Weather
-}
